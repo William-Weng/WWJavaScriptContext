cee4c74e066b35d1664fa444aeb03a0d464a7a47
William.Weng <William.Weng@iit.com.hk>
2023-08-23 14:33:35 +0800

    - v1.0.0

--- Example/Example/ViewController/ViewController.swift
+++ Example/Example/ViewController/ViewController.swift
@@ -1,23 +1,11 @@
-# WWJavaScriptContext
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by William.Weng on 2022/12/15.
+//  ~/Library/Caches/org.swift.swiftpm/
+//  file:///Users/william/Desktop/WWJavaScriptContext
 
-[![Swift-5.8](https://img.shields.io/badge/Swift-5.8-orange.svg?style=flat)](https://developer.apple.com/swift/) [![iOS-14.0](https://img.shields.io/badge/iOS-14.0-pink.svg?style=flat)](https://developer.apple.com/swift/) ![TAG](https://img.shields.io/github/v/tag/William-Weng/WWJavaScriptContext) [![Swift Package Manager-SUCCESS](https://img.shields.io/badge/Swift_Package_Manager-SUCCESS-blue.svg?style=flat)](https://developer.apple.com/swift/) [![LICENSE](https://img.shields.io/badge/LICENSE-MIT-yellow.svg?style=flat)](https://developer.apple.com/swift/)
-
-JavaScriptCore Widget.
-
-JavaScriptCore小工具.
-
-![WWJavaScriptContext](./Example.png)
-
-### [Installation with Swift Package Manager](https://medium.com/彼得潘的-swift-ios-app-開發問題解答集/使用-spm-安裝第三方套件-xcode-11-新功能-2c4ffcf85b4b)
-
-```bash
-dependencies: [
-    .package(url: "https://github.com/William-Weng/WWJavaScriptContext.git", .upToNextMajor(from: "1.0.0"))
-]
-```
-
-### Example
-```swift
 import UIKit
 import WWJavaScriptContext
 import WWPrint
@@ -75,6 +63,3 @@ private extension ViewController {
         wwPrint(context.callFunctionName("add", arguments: [1, 2]))
     }
 }
-```
-
-
